068498a5abf2e05ca701946923b3424d2ffb196d
Mario M. M <kingarpharazon@gmail.com>
2012-11-30 21:23:06 -0600

    Change project version to qt 4.8.4
    - Change the project version to qt 4.8.4
    - Restore color to states
    - Change de default size of the local version

--- caview.cpp
+++ caview.cpp
@@ -10,10 +10,10 @@ QWidget(parent), mDesiredWidth(ca->width() * 1), mDesiredHeight(ca->height() * 1
 		mCellularAutomaton->height(),  QImage::Format_Indexed8);
 
 	// Initialize the state table
-	mLatticeImage->setColor(0, qRgb(0, 0, 0));			// Empty
-	mLatticeImage->setColor(1, qRgb(130, 130, 130));	// Prey
-	mLatticeImage->setColor(2, qRgb(80, 80, 80));		// Predator
-	mLatticeImage->setColor(3, qRgb(255, 255, 255));		// Prey and predator
+	mLatticeImage->setColor(0, qRgb(0, 0, 0));		// Empty
+	mLatticeImage->setColor(1, qRgb(38, 127, 0));	// Prey
+	mLatticeImage->setColor(2, qRgb(255, 0, 0));	// Predator
+	mLatticeImage->setColor(3, qRgb(255, 216, 0));	// Prey and predator
 	mLatticeImage->setColor(4, qRgb(255, 0, 0));	// Global Best
 	mLatticeImage->setColor(5, qRgb(255, 0, 0));	// Global Best
 	mLatticeImage->setColor(6, qRgb(255, 0, 0));	// Global Best
