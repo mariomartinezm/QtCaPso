95ecd2ea62ca76140ea9e7545937b7a69c5b80e8
Mario M. M <mariomartinezmolina@live.com>
2013-01-30 19:00:37 -0600

    Implement new constructor of LocalSettingsDialog
    - The new constructor should populate the form using the pairs of the
      QMap received.
    
    - The QMap received in the constructor should be stored in a member var.
    
    - The LocalSettingsDialog::accept() method updates the settings object
      using the corresponding properties of the widgets.

--- localsettingsdialog.cpp
+++ localsettingsdialog.cpp
@@ -3,16 +3,34 @@
 #pragma warning(pop)
 #include "localsettingsdialog.h"
 
-LocalSettingsDialog::LocalSettingsDialog(QWidget* parent)
-	: QDialog(parent)
+LocalSettingsDialog::LocalSettingsDialog(QMap<QString, QVariant> settings,
+                                         QWidget *parent)
+    : QDialog(parent)
 {
-	this->setupUi(this);
+    this->setupUi(this);
 
-	// Set the application's path to the correspoinding lineEdit's text
-	lineEditPath->setText(QCoreApplication::applicationDirPath() + "/results.txt");
+    mSettings = settings;
 
-	// Connect the browse button to its corresponding slot
-	connect(pushButtonBrowse, SIGNAL(clicked()), this, SLOT(showFileDialog()));
+    // Populate form
+    lineEditInitialPreys->setText(mSettings["initialNumberOfPreys"].toString());
+    lineEditCompetenceFactor->setText(mSettings["competenceFactor"].toString());
+    spinBoxPreyReproductionRadius->setValue(mSettings["preyReproductionRadius"].toInt());
+    spinBoxPreyReproductiveCapacity->setValue(mSettings["preyReproductiveCapacity"].toInt());
+    lineEditInitialPredators->setText(mSettings["initialNumberOfPredators"].toString());
+    lineEditCognitiveFactor->setText(mSettings["predatorCognitiveFactor"].toString());
+    lineEditSocialFactor->setText(mSettings["predatorSocialFactor"].toString());
+    spinBoxMaxSpeed->setValue(mSettings["predatorMaximumSpeed"].toInt());
+    spinBoxPredatorReproductiveCapacity->setValue(mSettings["predatorReproductiveCapacity"].toInt());
+    spinBoxPredatorReproductionRadius->setValue(mSettings["predatorReproductionRadius"].toInt());
+    spinBoxSocialRadius->setValue(mSettings["predatorSocialRadius"].toInt());
+    spinBoxFitnessRadius->setValue(mSettings["fitnessRadius"].toInt());
+    lineEditInitialWeight->setText(mSettings["initialInertiaWeight"].toString());
+    lineEditFinalWeight->setText(mSettings["finalInertiaWeight"].toString());
+
+    lineEditPath->setText(mSettings["resultsFilePath"].toString());
+
+    // Connect the browse button to its corresponding slot
+    connect(pushButtonBrowse, SIGNAL(clicked()), this, SLOT(showFileDialog()));
 }
 
 LocalSettingsDialog::~LocalSettingsDialog()
@@ -27,32 +45,32 @@ void LocalSettingsDialog::showFileDialog()
 
 	if(!path.isEmpty())
 	{
-		lineEditPath->setText(path + "/results.txt");
+        lineEditPath->setText(path + "/results.txt");
 	}
 }
 
 void LocalSettingsDialog::accept()
 {
 	// Insert prey data
-	mSettings.insert("initialNumberOfPreys", lineEditInitialPreys->text().toFloat());
-	mSettings.insert("competenceFactor", lineEditCompetenceFactor->text().toFloat());
-	mSettings.insert("preyReproductionRadius", spinBoxPreyReproductionRadius->value());
-	mSettings.insert("preyReproductiveCapacity", spinBoxPreyReproductiveCapacity->value());
+    mSettings["initialNumberOfPreys"] =  lineEditInitialPreys->text().toFloat();
+    mSettings["competenceFactor"] =  lineEditCompetenceFactor->text().toFloat();
+    mSettings["preyReproductionRadius"] = spinBoxPreyReproductionRadius->value();
+    mSettings["preyReproductiveCapacity"] = spinBoxPreyReproductiveCapacity->value();
 
 	// Insert predator data
-	mSettings.insert("initialNumberOfPredators", lineEditInitialPredators->text().toInt());
-	mSettings.insert("predatorCognitiveFactor", lineEditCognitiveFactor->text().toFloat());
-	mSettings.insert("predatorSocialFactor", lineEditSocialFactor->text().toFloat());
-	mSettings.insert("predatorMaximumSpeed", spinBoxMaxSpeed->value());
-	mSettings.insert("predatorReproductiveCapacity", spinBoxPredatorReproductiveCapacity->value());
-	mSettings.insert("predatorReproductionRadius", spinBoxPredatorReproductionRadius->value());
-	mSettings.insert("predatorSocialRadius", spinBoxSocialRadius->value());
-	mSettings.insert("fitnessRadius", spinBoxFitnessRadius->value());
-	mSettings.insert("initialInertiaWeight", lineEditInitialWeight->text().toFloat());
-	mSettings.insert("finalInertiaWeight", lineEditFinalWeight->text().toFloat());
+    mSettings["initialNumberOfPredators"] = lineEditInitialPredators->text().toInt();
+    mSettings["predatorCognitiveFactor"] = lineEditCognitiveFactor->text().toFloat();
+    mSettings["predatorSocialFactor"] = lineEditSocialFactor->text().toFloat();
+    mSettings["predatorMaximumSpeed"] = spinBoxMaxSpeed->value();
+    mSettings["predatorReproductiveCapacity"] = spinBoxPredatorReproductiveCapacity->value();
+    mSettings["predatorReproductionRadius"] = spinBoxPredatorReproductionRadius->value();
+    mSettings["predatorSocialRadius"] = spinBoxSocialRadius->value();
+    mSettings["fitnessRadius"] = spinBoxFitnessRadius->value();
+    mSettings["initialInertiaWeight"] = lineEditInitialWeight->text().toFloat();
+    mSettings["finalInertiaWeight"] = lineEditFinalWeight->text().toFloat();
 
 	// Insert path to results file
-	mSettings.insert("resultsFilePath", lineEditPath->text());
+    mSettings["resultsFilePath"] = lineEditPath->text();
 
 	emit settingsChanged(mSettings);
 
