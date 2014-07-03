6cea48e700c712acb310a9e50ab76ba60f4f1346
kingarpharazon@numenor <mariomartinezmolina@live.com>
2014-07-02 19:31:25 -0500

    Fix a compilation problem with mingw
    Mingw needs the same signature as gcc for the Swarm's class erase
    method.

--- swarm.h
+++ swarm.h
@@ -10,41 +10,44 @@
 class Swarm
 {
 public:
-	Swarm(float cognitiveFactor, float socialFactor, int maxSpeed);
+    Swarm(float cognitiveFactor, float socialFactor, int maxSpeed);
 
-	~Swarm();
+    ~Swarm();
 
-	float cognitiveFactor() const { return mCognitiveFactor; }
-	float socialFactor() const { return mSocialFactor; }
-	int maxSpeed() const { return mMaxSpeed; }
-	float inertiaWeight() const { return mInertiaWeight; }
+    float cognitiveFactor() const { return mCognitiveFactor; }
+    float socialFactor() const { return mSocialFactor; }
+    int maxSpeed() const { return mMaxSpeed; }
+    float inertiaWeight() const { return mInertiaWeight; }
 
-	void setCognitiveFactor(float factor) { mCognitiveFactor = factor; }
-	void setSocialFactor(float factor) { mSocialFactor = factor; }
-	void setMaxSpeed(int speed) { mMaxSpeed = speed; }
+    void setCognitiveFactor(float factor) { mCognitiveFactor = factor; }
+    void setSocialFactor(float factor) { mSocialFactor = factor; }
+    void setMaxSpeed(int speed) { mMaxSpeed = speed; }
 
-	std::list<std::shared_ptr<Particle>>::iterator begin();
-	std::list<std::shared_ptr<Particle>>::iterator end();
+    std::list<std::shared_ptr<Particle>>::iterator begin();
+    std::list<std::shared_ptr<Particle>>::iterator end();
 
-	void initialize(int size, int width, int height, std::mt19937& random);
-	void add(std::list<std::shared_ptr<Particle>>& newParticles);
+    void initialize(int size, int width, int height, std::mt19937& random);
+    void add(std::list<std::shared_ptr<Particle>>& newParticles);
 
 #if defined(Q_OS_LINUX)
-	std::list<std::shared_ptr<Particle>, std::allocator<std::shared_ptr<Particle>>>::iterator erase(std::list<std::shared_ptr<Particle>,
+    std::list<std::shared_ptr<Particle>, std::allocator<std::shared_ptr<Particle>>>::iterator erase(std::list<std::shared_ptr<Particle>,
+        std::allocator<std::shared_ptr<Particle>>>::iterator it);
+#elif defined(__MINGW32__)
+    std::list<std::shared_ptr<Particle>, std::allocator<std::shared_ptr<Particle>>>::iterator erase(std::list<std::shared_ptr<Particle>,
         std::allocator<std::shared_ptr<Particle>>>::iterator it);
 #else
     std::list<std::shared_ptr<Particle>, std::allocator<std::shared_ptr<Particle>>>::iterator erase(std::list<std::shared_ptr<Particle>,
         std::allocator<std::shared_ptr<Particle>>>::const_iterator it);
 #endif
 
-	bool empty() const;
+    bool empty() const;
 
 private:
-	std::list<std::shared_ptr<Particle>> mParticles;
-	float mCognitiveFactor;
-	float mSocialFactor;
-	int mMaxSpeed;
-	float mInertiaWeight;
+    std::list<std::shared_ptr<Particle>> mParticles;
+    float mCognitiveFactor;
+    float mSocialFactor;
+    int mMaxSpeed;
+    float mInertiaWeight;
 };
 
 #endif // SWARM_H
