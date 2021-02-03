2c6297d8ad66c21eaa6cc2b31ada18aadb5ca639
kingarpharazon@armenelos <kingarpharazon@live.com.mx>
2021-02-03 09:16:10 -0600

    Add test project
    Add simple tests for random number wrapper.

---
+++ test/randomnumber-test.cpp
@@ -0,0 +1,44 @@
+#include "gtest/gtest.h"
+#include "Models/randomnumber.h"
+
+TEST(RandomNumber, test_float_limits)
+{
+    RandomNumber rand;
+
+    bool error = false;
+
+    for(int i = 0; i < 1000; i++)
+    {
+        auto r = rand.GetRandomFloat();
+
+        if(r < 0 || r > 1)
+        {
+            error = true;
+            break;
+        }
+    }
+
+    EXPECT_EQ(error, false);
+}
+
+TEST(RandomNumber, test_integer_interval)
+{
+    RandomNumber rand;
+
+    bool error = false;
+
+    for(int i = 0; i < 1000; i++)
+    {
+        auto a = -10;
+        auto b = 5;
+        auto r = rand.GetRandomInt(a, b);
+
+        if(r < a || r > b)
+        {
+            error = true;
+            break;
+        }
+    }
+
+    EXPECT_EQ(error, false);
+}
