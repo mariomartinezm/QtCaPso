9d10d70b85de30d4851ed1c5f7200ac7f30a7a64
mariomm <mariomartinezmolina@live.com>
2021-12-13 23:09:00 -0600

    Update RandomNumber class
    Fix seeding of the pcg32 engine. Since MinGW provides no implementation
    of random_device and the high_resolution_clock is not accurate enought,
    it is necessary to use steady_clock as a workaround.

--- src/Models/randomnumber.cpp
+++ src/Models/randomnumber.cpp
@@ -1,31 +1,28 @@
-#include "randomnumber.h"
-
 #include <chrono>
+#include "randomnumber.h"
 
 RandomNumber::RandomNumber()
     : mRealDistribution(0.0, 1.0)
 {
     // Initialize PRNG
     std::random_device rd;
-    unsigned seed;
 
     // Check if the implementation provides a usable random_device
     if (0 != rd.entropy())
     {
-        seed = rd();
+        pcg_extras::seed_seq_from<std::random_device> seed_source;
+        mRNG = std::make_unique<pcg32>(seed_source);
     }
     else
     {
-        // No random device available, seed using the system clock
-        seed = static_cast<unsigned> (std::chrono::system_clock::now().time_since_epoch().count());
+        // No random device available, seed using the system clock. Note however
+        // that in MinGW std::chrono::high_resolution_clock is not accurate
+        // enough. Thus threads that start close to one another might have
+        // the same seed. A workaround seems to use std::chrono::steady_clock
+        // instead.
+        uint64_t seed = static_cast<unsigned> (std::chrono::steady_clock::now().time_since_epoch().count());
+        mRNG = std::make_unique<pcg32>(seed);
     }
-
-    mRNG = new pcg32(seed);
-}
-
-RandomNumber::~RandomNumber()
-{
-    delete mRNG;
 }
 
 float RandomNumber::GetRandomFloat()
