95450db095a125292a5d38a1a6ddc6e0828a3ece
kingarpharazon <kingarpharazon@live.com.mx>
2022-10-13 17:20:30 -0500

    Update util.h
    Add struct CaPsoSettings.
    
    This type groups all the parameters for a local CaPso model. This will
    allow to simplify the signature and inner workings of several functions.

--- src/util.h
+++ src/util.h
@@ -6,6 +6,24 @@
 
 namespace util
 {
+    struct CaPsoSettings
+    {
+        float initialPreyDensity         = { 0.3F };
+        float competitionFactor          = { 0.3F };
+        int preyReproductionRadius       = { 2 };
+        int preyReproductiveCapacity     = { 10 };
+        int fitnessRadius                = { 3 };
+        int initialPredatorSwarmSize     = { 3 };
+        float predatorCognitiveFactor    = { 1.0F };
+        float predatorSocialFactor       = { 2.0F };
+        int predatorMaxSpeed             = { 10 };
+        int predatorReproductiveCapacity = { 10 };
+        int predatorReproductionRadius   = { 2 };
+        int predatorSocialRadius         = { 3 };
+        float initialInertiaWeight       = { 0.9F };
+        float finalInertiaWeight         = { 0.2F };
+    };
+
     bool loadSettings(CellularAutomaton* ca, CaType type, QString settingsFilename);
 
     bool writeSettings(CaType type=LOCAL,
