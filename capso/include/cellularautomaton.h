978c920e64b7b6ba6e512a227ae6507175366402
mariomm <mariomartinezmolina@live.com>
2023-07-14 14:04:26 -0600

    Update capso library
    Add CellularAutomaton class to the capso namespace.

--- capso/include/cellularautomaton.h
+++ capso/include/cellularautomaton.h
@@ -3,37 +3,40 @@
 
 #include <vector>
 
-class CellularAutomaton
+namespace capso
 {
-public:
-    CellularAutomaton(int width, int height);
+    class CellularAutomaton
+    {
+    public:
+        CellularAutomaton(int width, int height);
 
-    unsigned char* latticeData() const;
+        unsigned char* latticeData() const;
 
-    int width() const;
+        int width() const;
 
-    int height() const;
+        int height() const;
 
-    void setCellState(int row, int col, unsigned char state);
+        void setCellState(int row, int col, unsigned char state);
 
-    virtual void clear();
+        virtual void clear();
 
-    virtual void initialize() = 0;
+        virtual void initialize() = 0;
 
-    virtual void nextGen() = 0;
+        virtual void nextGen() = 0;
 
-    virtual ~CellularAutomaton() = 0;
+        virtual ~CellularAutomaton() = 0;
 
-protected:
-    std::vector<unsigned char> mLattice;
-    int mWidth, mHeight, mStride;
+    protected:
+        std::vector<unsigned char> mLattice;
+        int mWidth, mHeight, mStride;
 
-    int getAddress(int row, int col);
-};
+        int getAddress(int row, int col);
+    };
 
-inline int CellularAutomaton::getAddress(int row, int col)
-{
-    return mStride * row + col;
+    inline int CellularAutomaton::getAddress(int row, int col)
+    {
+        return mStride * row + col;
+    }
 }
 
 #endif // CELLULARAUTOMATON_H
